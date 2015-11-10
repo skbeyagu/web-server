cf96ca7d5278e190c624c063634baecb3eea7cd1
Santosh Beyagudem <skbeyagu@gmail.com>
2015-11-09 19:20:38 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next){
+		
+		console.log(new Date().toString() +' '+req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+//app.use(middleware.requireAuthentication);
+
+app.use(middleware.logger);
+
+app.listen(PORT, function (){
+	console.log('Express server started on port '+ PORT);
+});
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
