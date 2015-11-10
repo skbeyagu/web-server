7a01a199c415c1cb9ae6f9357dfef7ff0b1b560f
Santosh Beyagudem <skbeyagu@gmail.com>
2015-11-09 20:05:08 -0500

    Updated Heroku Port

---
+++ middleware.js
@@ -0,0 +1,17 @@
+
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		
+		console.log('private route hit!!');
+		next();
+	},
+	logger: function (req, res, next){
+		
+		console.log(new Date().toString() +' '+req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+
+module.exports = middleware;
